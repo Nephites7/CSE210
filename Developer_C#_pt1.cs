50fbd073f686953e713faa648b16aaeb03b3ac6f
Nephites7 <sashinshim27@gmail.com>
2023-10-21 11:45:55 -0600

    https://github.com/Nephites7/CSE210

---
+++ Developer_C#_pt1.cs
@@ -0,0 +1,25 @@
+
+// ```csharp
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("What is your first name? ");
+        string first_Name = Console.ReadLine();
+
+        
+        Console.Write("What is your last name? ");
+        string last_Name = Console.ReadLine();
+
+        
+        Console.WriteLine($"Your name is {last_Name}, {firs_tName} {last_Name}.");
+
+        
+        Console.ReadLine();
+    }
+}
+// ```
+
---
+++ Developer_C#_pt2.cs
@@ -0,0 +1,52 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Enter your grade percentage: ");
+        double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        
+        char letter;
+
+        
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep up the good work and try again next time.");
+        }
+
+        
+        Console.ReadLine();
+    }
+}
